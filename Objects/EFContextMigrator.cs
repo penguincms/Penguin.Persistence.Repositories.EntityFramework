818fc815f006bcc40d88c371dc9e7e23d8cf1ec9
anloffredo <loffredo.ap@gmail.com>
2019-09-22 11:36:54 -0700

    Implementing DB migrator
    Implementing DB migrator

---
+++ Objects/EFContextMigrator.cs
@@ -0,0 +1,60 @@
+﻿using Penguin.Persistence.Abstractions;
+using Penguin.Persistence.Abstractions.Interfaces;
+using Penguin.Reflection;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity.Infrastructure;
+using System.Data.Entity.Migrations;
+using System.Diagnostics.Contracts;
+using System.Linq;
+using System.Text;
+
+namespace Penguin.Persistence.Repositories.EntityFramework.Objects
+{
+    /// <summary>
+    /// A class designed to initialize and update the database schema
+    /// </summary>
+    public class EFContextMigrator : IPersistenceContextMigrator
+    {
+        PersistenceConnectionInfo PersistenceConnectionInfo { get; set; }
+
+        /// <summary>
+        /// Constructs a new instance of the migrator using the provided connection string
+        /// </summary>
+        /// <param name="connectionInfo"></param>
+        public EFContextMigrator(PersistenceConnectionInfo connectionInfo)
+        {
+            Contract.Requires(connectionInfo != null);
+
+            PersistenceConnectionInfo = connectionInfo;
+        }
+
+        /// <summary>
+        /// Updates the database to reflect the current EntityFramework Schema
+        /// </summary>
+        public void Migrate()
+        {
+            
+
+            foreach (Type t in TypeFactory.GetDerivedTypes(typeof(DbMigrationsConfiguration)).Where(t => !t.ContainsGenericParameters))
+            {
+                //Allow for overridden migration configs via inheritence
+                Type derived = TypeFactory.GetMostDerivedType(t);
+
+                if (derived is null || derived == t)
+                {
+                    DbMigrationsConfiguration configuration = Activator.CreateInstance(t) as DbMigrationsConfiguration;
+
+                    if (configuration.AutomaticMigrationsEnabled)
+                    {
+                        configuration.TargetDatabase = new DbConnectionInfo(PersistenceConnectionInfo.ConnectionString, PersistenceConnectionInfo.ProviderName);
+
+                        DbMigrator migrator = new DbMigrator(configuration);
+
+                        migrator.Update();
+                    }
+                }
+            }
+        }
+    }
+}
